cc7ff46539dc31b86097088d7442a115fb6e9abe
Zweli Golowa <itszweli@hotmail.com>
2024-08-12 14:41:12 +0200

    Reverted the change on number of samples will do it later

--- dataUpdater/data_pull.py
+++ dataUpdater/data_pull.py
@@ -98,14 +98,14 @@ def pull_direct():
     headers["Cache-Control"] = "max-age=0"
     headers["Connection"] = "keep-alive"
     headers["Content-Type"] = "application/x-www-form-urlencoded"
-    headers["Cookie"] = "username=riders; password=health; searchPanel=%7B%22panelState_sample_collection_1%22%3A%7B%22srchPanelOpen%22%3Afalse%2C%22srchCtrlComboOpen%22%3Afalse%2C%22srchWinOpen%22%3Afalse%2C%22openFilters%22%3A%5B%5D%7D%7D; s1574928373=45b05dd296a722d89fe7d2c1dc160deb"
+    headers["Cookie"] = "searchPanel=%7B%22panelState_sample_collection_1%22%3A%7B%22srchPanelOpen%22%3Afalse%2C%22srchCtrlComboOpen%22%3Afalse%2C%22srchWinOpen%22%3Afalse%2C%22openFilters%22%3A%5B%5D%7D%7D; username=riders; password=health; s1574928373=d5b799606ae8c3085a479423a5b14748"
     headers["Origin"] = "http://206.225.84.201"
     headers["Referer"] = "http://206.225.84.201/riders_test/sample_collection_export.php"
     headers["Sec-GPC"] = "1"
     headers["Upgrade-Insecure-Requests"] = "1"
     headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
 
-    data = "type=csv&records=page"
+    data = "type=csv&records=all&rndVal=0.28999320348498614"
 
     print("Downloading reported volumes")
     resp = requests.post(url, headers=headers, data=data)
